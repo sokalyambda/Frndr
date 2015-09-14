83dad13c26b52b641ff879e85ee7b1f05e210225
Pavlo Muratov <Pavlo@Pavlos-MacBook-Pro.local>
2015-09-14 12:17:02 +0300

    + FRDSearchSettingController: search setting screen and controller added

--- Frndr/Classes/MVC/Controllers/SearchSettingsController/FRDSearchSettingsController.h
+++ Frndr/Classes/MVC/Controllers/SearchSettingsController/FRDSearchSettingsController.h
@@ -1,11 +1,11 @@
 //
-//  FRDSplashScreenController.h
+//  FRDSearchSettingsController.h
 //  Frndr
 //
 //  Created by Pavlo on 9/14/15.
 //  Copyright (c) 2015 ThinkMobiles. All rights reserved.
 //
 
-@interface FRDAutologinViewController : UIViewController
+@interface FRDSearchSettingsController : UIViewController
 
 @end
---
+++ Frndr/Classes/MVC/Controllers/SearchSettingsController/FRDSearchSettingsController.m
@@ -0,0 +1,13 @@
+//
+//  FRDSearchSettingsController.m
+//  Frndr
+//
+//  Created by Pavlo on 9/14/15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import "FRDSearchSettingsController.h"
+
+@implementation FRDSearchSettingsController
+
+@end
