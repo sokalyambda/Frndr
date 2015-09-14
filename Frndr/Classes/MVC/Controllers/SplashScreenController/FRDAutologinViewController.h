8fcfbe88bbd25003e3e983abf6a204b58cbc21ae
Pavlo Muratov <Pavlo@Pavlos-MacBook-Pro.local>
2015-09-14 09:57:04 +0300

    + FRDAutologinViewController: autologin (splash) screen added

---
+++ Frndr/Classes/MVC/Controllers/SplashScreenController/FRDAutologinViewController.h
@@ -0,0 +1,13 @@
+//
+//  FRDSplashScreenController.h
+//  Frndr
+//
+//  Created by Pavlo on 9/14/15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRDAutologinViewController : UIViewController
+
+@end
---
+++ Frndr/Classes/MVC/Controllers/SplashScreenController/FRDAutologinViewController.m
@@ -0,0 +1,42 @@
+//
+//  FRDSplashScreenController.m
+//  Frndr
+//
+//  Created by Pavlo on 9/14/15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import "FRDAutologinViewController.h"
+
+static NSString *const kTutorialSegueIdentifier = @"tutorialSegueIdentifier";
+
+@implementation FRDAutologinViewController
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self moveToTutorialAfterDelay:0.f];
+}
+
+#pragma mark - Actions
+
+- (void)moveToTutorialAfterDelay:(CGFloat)delay
+{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self performSegueWithIdentifier:kTutorialSegueIdentifier sender:self];
+    });
+}
+
+- (void)customizeNavigationItem
+{
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleDefault;
+}
+
+@end
