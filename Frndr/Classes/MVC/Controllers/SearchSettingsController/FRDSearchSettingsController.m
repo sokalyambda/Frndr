fdc5d07b3cb94ce8b19ef5cc19ee5479f9e0aa5f
Eugenity <Eugenity@MacBook-Pro-MacBook.local>
2015-09-15 11:31:50 +0300

    minor ui changes

--- Frndr/Classes/MVC/Controllers/SearchSettingsController/FRDSearchSettingsController.m
+++ Frndr/Classes/MVC/Controllers/SearchSettingsController/FRDSearchSettingsController.m
@@ -14,7 +14,12 @@
 
 #import "FRDBaseDropDownDataSource.h"
 
+#import "FRDSerialViewConstructor.h"
+
 @interface FRDSearchSettingsController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *smokerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sexualOrientationLabel;
 
 @property (nonatomic) FRDDropDownTableView *dropDownList;
 
@@ -30,14 +35,22 @@
     [self initDropDownTable];
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    
+    [super viewWillAppear:animated];
+}
+
 #pragma mark - Actions
 
 - (IBAction)smokerClick:(UITapGestureRecognizer *)tap
 {
     UIView *tapView = tap.view;
     FRDBaseDropDownDataSource *smokerDataSource = [FRDBaseDropDownDataSource dataSourceWithType:FRDDataSourceTypeSmoker];
+    WEAK_SELF;
     [self.dropDownList dropDownTableBecomeActiveInView:self.view fromAnchorView:tapView withDataSource:smokerDataSource withCompletion:^(FRDDropDownTableView *table, NSString *chosenValue) {
         NSLog(@"chosen value %@", chosenValue);
+        weakSelf.smokerLabel.text = chosenValue;
     }];
 }
 
@@ -45,14 +58,29 @@
 {
     UIView *tapView = tap.view;
     FRDBaseDropDownDataSource *smokerDataSource = [FRDBaseDropDownDataSource dataSourceWithType:FRDDataSourceTypeSexualOrientation];
+    WEAK_SELF;
     [self.dropDownList dropDownTableBecomeActiveInView:self.view fromAnchorView:tapView withDataSource:smokerDataSource withCompletion:^(FRDDropDownTableView *table, NSString *chosenValue) {
         NSLog(@"chosen value %@", chosenValue);
+        weakSelf.sexualOrientationLabel.text = chosenValue;
     }];
 }
 
 - (void)initDropDownTable
 {
     self.dropDownList = [FRDDropDownTableView makeFromXib];
+}
+
+- (void)customizeNavigationItem
+{
+    [super customizeNavigationItem];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    
+    self.navigationTitleView.titleText = LOCALIZED(@"Search Settings");
+    
+    UIBarButtonItem *rightIcon = [FRDSerialViewConstructor customRightBarButtonForController:self withAction:nil];
+    
+    //set right bar button item
+    self.navigationItem.rightBarButtonItem = rightIcon;
 }
 
 @end
